949ba43de7234bfbac32d6a1907c010d4b330abb
datmedevil17 <rakshitshukla17@gmail.com>
2026-02-10 16:44:16 +0530

    new deployment

--- sol/programs/candy/src/lib.rs
+++ sol/programs/candy/src/lib.rs
@@ -14,7 +14,7 @@ use mpl_token_metadata::types::DataV2;
 use session_keys::{session_auth_or, Session, SessionError, SessionToken};
 
 
-declare_id!("HEPEoHjEb2tJRgzHdWNUmqpojX11nYTJWAhx3QBxVhiY");
+declare_id!("6DKEYNYdiTB77U36Gnkp5kGYqJPmeUTyqJuavhejrF6t");
 
 // ========================================
 // Level Configuration (On-chain Constants)
